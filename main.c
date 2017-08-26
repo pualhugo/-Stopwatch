d209cb95da687442ef1f4dba7d6766ddb770376e
pualhugo <1124133164@qq.com>
2017-08-26 16:07:17 +0800

    Add files via upload
    sourcefile

---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+typedef unsigned int uint;
+void time_clock();
+int main()
+{
+    printf("按下s键开始计时");
+    char ch=getchar();
+    if(ch=='s')
+        time_clock();
+    else if(ch==27)
+        exit(0);
+    return 0;
+}
+void time_clock()
+{
+    uint hour=0,min=0,sed=0;
+    do
+    {
+        printf("%dhour%dmin%dsed",hour,min,sed);
+        sleep(1);
+        sed++;
+        system("cls");
+        if(sed>59)
+        {
+            min++;
+            sed=0;
+        }
+        if(min>59)
+        {
+            hour++;
+            min=0;
+        }
+    }while(1);
+
+}
