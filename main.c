cf76a16dfc807ece601b070aa96210de838b8f7a
pualhugo <1124133164@qq.com>
2017-08-26 16:32:15 +0800

    Update main.c

--- main.c
+++ main.c
@@ -9,25 +9,25 @@ int main()
     char ch=getchar();
     if(ch == 's')
         time_clock();
-    else if(ch == 27)
+    else if(ch == 27)   //退出格式
         exit(0);
     return 0;
 }
 void time_clock()
 {
-    uint hour = 0,min = 0,sed = 0;
+    uint hour = 0,min = 0,sed = 0;  //初始化元素
     do
     {
         printf("%dhour%dmin%dsed",hour,min,sed);
-        sleep(1);
+        sleep(1);   //延时一秒
         sed++;
-        system("cls");
+        system("cls");  //清屏
         if(sed > 59)
         {
             min++;
             sed = 0;
         }
-        if(min>59)
+        if(min > 59)
         {
             hour++;
             min = 0;
